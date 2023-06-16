91d59e983ab5c92cc99fb68bcc8695dbf3ff7586
mwaller129 <mwaller129@gmail.com>
2023-06-16 14:46:27 -0400

    waller

--- showtime/main_app/models.py
+++ showtime/main_app/models.py
@@ -1,17 +1,34 @@
 from django.db import models
+from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
 
 
+class Theater(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+   
+
+    def __str__(self):
+        return f"{self.name} ({self.date})"
+
 class Show(models.Model):
     name = models.CharField(max_length=100)
-    theater = models.CharField(max_length=100)
+    theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
     date = models.DateField('Show Date')
-    review = models.TextField(max_length=250)
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
 
-    # def get_absolute_url(self):
-    #     return reverse('detail', kwargs={'show_id': self.id})
+    def get_absolute_url(self):
+       return reverse('detail', kwargs={'show_id': self.id})
+
+class Review(models.Model):
+    date = models.DateField('Review Date')
+    description = models.TextField(max_length=250)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_review_display()} on {self.date}"
+
--- showtime/main_app/urls.py
+++ showtime/main_app/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
 
      path('', views.home, name='home'),
-     path('about/', views.about, name='about'),
      path('shows/', views.shows_index, name='index'),
-     path('shows/<int:show_id>/', views.shows.detail, name='detail'),
+     path('shows/<int:show_id>/', views.shows_detail, name='detail'),
+     path('shows/create/', views.ShowCreate.as_view(), name='shows_create'),
+     path('shows/<int:pk>/update', views.ShowUpdate.as_view(), name='shows_update'),
+     path('shows/<int:pk>/delete', views. ShowDelete.as_view(), name='shows_delete'),
      path('accounts.signup/', views.signup, name='signup')
 ]
 
