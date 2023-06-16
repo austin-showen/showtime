f492c9d22cb7c5aef4762181c86755cfff953402
mwaller129 <mwaller129@gmail.com>
2023-06-16 13:02:55 -0400

    waller

--- showtime/main_app/urls.py
+++ showtime/main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
      path('', views.home, name='home'),
-     path('shows/', views.shows_index, name='index')
+     path('about/', views.about, name='about'),
+     path('shows/', views.shows_index, name='index'),
+     path('shows/<int:show_id>/', views.shows.detail, name='detail'),
+     path('accounts.signup/', views.signup, name='signup')
 ]
