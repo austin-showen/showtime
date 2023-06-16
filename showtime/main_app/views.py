5a3931e4c8cf13ed5c08f4fbfb6141bc747d9325
austin-showen <austin.showen@gmail.com>
2023-06-16 10:41:12 -0500

    create basic signup page

--- showtime/main_app/views.py
+++ showtime/main_app/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, "home.html")
+
+
+def signup(request):
+    error_message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            error_message = "Invalid signup - try again"
+    form = UserCreationForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)
