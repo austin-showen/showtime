4e257e255926b8a82bb942508ef944548b3640e7
mwaller129 <mwaller129@gmail.com>
2023-06-16 11:44:23 -0400

    waller

--- showtime/main_app/views.py
+++ showtime/main_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'home.html')
+
+def shows_index(request):
+    return render(request, 'shows/index.html', {
+        'shows' : shows
+    })
